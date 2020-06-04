bacda923f72c78c6363987555816d24d2d01488b
ajay-a27 <66016053+ajay-a27@users.noreply.github.com>
2020-06-04 23:14:53 +0530

    Create factorial.java

---
+++ day 6/factorial.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import  java.util.*;
+class usermaincode
+{
+public int nfactorial(int input1)
+{
+int i;int fact=1;
+
+		for(i=1;i<=input1;i++)
+		{
+			fact*=i;
+		}
+		return fact;
+	}
+}
