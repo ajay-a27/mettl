af527204db4dca500584cbf7c973ce3700c535c4
ajay-a27 <66016053+ajay-a27@users.noreply.github.com>
2020-06-06 13:19:05 +0530

    Create sum_of_odd_digit.java

---
+++ day 8/sum_of_odd_digit.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import  java.util.*;
+class Usermaincode
+{
+public int odddigitsum(int input1)
+{
+	int temp=0,sum=0;
+		while(input1>0)
+		{
+			temp=input1%10;
+			if(temp%2!=0)
+			{
+				sum+=temp;
+			}
+			input1/=10;
+		}
+		return sum;
+	}
+}
